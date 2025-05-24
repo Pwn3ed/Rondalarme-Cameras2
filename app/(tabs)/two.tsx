b08b9d0058d60b341a6a8a6b64cc84aafa5bf591
Diego Michel Prestes <diegoprestes99@hotmail.com>
2025-05-24 18:12:44 -0300

    [config] first build android

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,32 +1,90 @@
 import { StyleSheet } from 'react-native';
 import { Text, View } from '@/components/Themed';
-import { RTCPeerConnection, RTCView } from 'react-native-webrtc';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import {
+	ScreenCapturePickerView,
+	RTCPeerConnection,
+	RTCIceCandidate,
+	RTCSessionDescription,
+	RTCView,
+	MediaStream,
+	MediaStreamTrack,
+	mediaDevices,
+	registerGlobals
+} from 'react-native-webrtc';
 
 export default function TabTwoScreen() {
 
   const [streamUrl, setStreamUrl] = useState(null);
+  const localStream = useRef<MediaStream | null>(null);
+
+  // useEffect(() => {
+  //      const getMedia = async () => {
+  //        const stream = await mediaDevices.getUserMedia({
+  //          video: true,
+  //          audio: true,
+  //        });
+  //        localStream.current = stream;
+  //      };
+
+  //      getMedia();
+  //    }, []);
 
   useEffect(() => {
-    const pc = new RTCPeerConnection({
-      iceServers: [
-        { urls: 'stun:stun.l.google.com:19302' },
-      ]
-    })
+    const run = async () => {
+      const pc = new RTCPeerConnection({
+        iceServers: [
+          { urls: 'stun:stun.l.google.com:19302' },
+        ]
+      })
 
-    
+      let sessionConstraints = {
+        mandatory: {
+          OfferToReceiveAudio: true,
+          OfferToReceiveVideo: true,
+          VoiceActivityDetection: true
+        }
+      };
 
-    
-  })
+      try {
+        const offerDescription = await pc.createOffer(sessionConstraints);
+        await pc.setLocalDescription(offerDescription);
+      } catch (err) {
+        console.log('ERROR' + err)
+      }
+
+      const ws = new WebSocket('ws://rondagprs.ddns.net:8889/app/stream');
+
+      ws.onopen = () => {
+        ws.send(JSON.stringify({
+          type: 'offer',
+          sdp: pc.localDescription?.sdp
+        }))
+      }
+
+      ws.onmessage = async (e) => {
+        const data = JSON.parse(e.data);
+        if (data.type === 'answer') {
+          await pc.setRemoteDescription(new RTCSessionDescription(data))
+        }
+      }
+
+      
+
+    };
+
+    run();
+    console.log(localStream.current?.toURL())
+  }, [])
 
   return (
     <View style={styles.container}>
       {
         streamUrl && (
           <RTCView
-            streamURL={streamUrl}
+            streamURL={localStream.current?.toURL()}
             style={styles.video}
-            objectFit='cover'
+            objectFit={'cover'}
           />
         )
       }
